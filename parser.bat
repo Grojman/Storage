59cec3a32fafa3c1e37842874638ea6c92465ff1
Grojman <gomisromanjorge@gmail.com>
2025-12-07 14:26:35 +0100

    Hoping that this is the last commit regarding changing something because of a trailing comma

--- parser.bat
+++ parser.bat
@@ -7,8 +7,19 @@ set JSON_FILE=repository-data.json
 :: Inicializamos JSON
 echo [> %JSON_FILE%
 
+:: Contar cuántos lenguajes hay
+set LANG_COUNT=0
+for /d %%L in (storage\*) do (
+    set /a LANG_COUNT+=1
+)
+
+:: Inicializamos índice de lenguajes
+set LANG_INDEX=0
+
 :: Recorremos cada lenguaje en storage
 for /d %%L in (storage\*) do (
+    set /a LANG_INDEX+=1
+
     set "LANG_NAME=%%~nxL"
     set "LANG_ICON=icons/%%~nxL.png"
     echo     {>> %JSON_FILE%
@@ -16,29 +27,35 @@ for /d %%L in (storage\*) do (
     echo         "icon": "!LANG_ICON!",>> %JSON_FILE%
     echo         "files": [>> %JSON_FILE%
 
-    set FIRST_FILE=1
-
-    :: Recorremos cada archivo en la carpeta del lenguaje
+    :: Contar archivos válidos (no .txt)
+    set COUNT=0
     for %%F in ("%%L\*.*") do (
-        :: Ignoramos archivos .txt
         if /i not "%%~xF"==".txt" (
-            :: Construimos ruta del .txt
-            set TXT_FILE=%%F.txt
+            set /a COUNT+=1
+        )
+    )
 
-            :: Si no existe el txt, pedimos al usuario
+
+    :: Inicializamos índice
+    set INDEX=0
+
+    :: Recorremos archivos válidos
+    for %%F in ("%%L\*.*") do (
+        if /i not "%%~xF"==".txt" (
+            set /a INDEX+=1
+
+            :: Construir path para JSON
+            set "FILE_PATH=%%F"
+            set "FILE_PATH=!FILE_PATH:\=/!"
+
+            :: Si no existe el .txt, pedirlo
+            set TXT_FILE=%%F.txt
             if not exist "!TXT_FILE!" (
                 set /p DESC="No existe descripcion para %%~nxF. Escribe la descripcion: "
                 echo !DESC!> "!TXT_FILE!"
             )
 
-            :: Agregamos la coma si no es el primer archivo
-            if !FIRST_FILE! equ 1 (
-                set FIRST_FILE=0
-            ) else (
-                echo ,>> %JSON_FILE%
-            )
-
-            :: Escapamos comillas en la descripción
+            :: Leer descripción (escapando comillas)
             set "DESC_TEXT="
             for /f "usebackq delims=" %%A in ("!TXT_FILE!") do (
                 set "LINE=%%A"
@@ -46,20 +63,28 @@ for /d %%L in (storage\*) do (
                 set "DESC_TEXT=!DESC_TEXT!!LINE! "
             )
 
-            set "FILE_PATH=%%F"
-            set "FILE_PATH=!FILE_PATH:\=/!" 
-
-            :: Escribimos el archivo en JSON
+            :: Escribir JSON
             echo             {>> %JSON_FILE%
             echo                 "name": "%%~nF",>> %JSON_FILE%
             echo                 "path": "!FILE_PATH!",>> %JSON_FILE%
             echo                 "description": "!DESC_TEXT:~0,-1!" >> %JSON_FILE%
             echo             }>> %JSON_FILE%
+
+            :: Añadir coma si NO es el último archivo
+            if !INDEX! lss !COUNT! (
+                echo ,>> %JSON_FILE%
+            )
         )
     )
 
+
     echo         ]>> %JSON_FILE%
-    echo     },>> %JSON_FILE%
+    echo     }>> %JSON_FILE%
+
+    :: Añadir coma si NO es el último lenguaje
+    if !LANG_INDEX! lss !LANG_COUNT! (
+        echo ,>> %JSON_FILE%
+    )
 )
 
 :: Quitamos la última coma (opcional, para JSON válido)
